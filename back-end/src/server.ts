a3c763db11f83b7fe4d2585b52f2aa38d41e80ab
sirmd <mate.deitos@hotmail.com>
2020-07-05 19:18:51 -0300

    Nivel 03 -> ReactJS -> Importando CORS no back-end

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes';
 import 'reflect-metadata';
@@ -7,6 +8,7 @@ import uploadConfig from './config/upload';
 import AppError from './errors/AppError';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
