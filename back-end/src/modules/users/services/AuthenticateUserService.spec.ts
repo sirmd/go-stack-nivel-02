e1c6b0d8bc9e33d1d59e813d29fd0b9f20b4a22b
sirmd <mate.deitos@hotmail.com>
2020-07-15 23:33:07 -0300

    Nivel 04 -> Node.JS -> feature: Outros testes de autenticação.

--- back-end/src/modules/users/services/AuthenticateUserService.spec.ts
+++ back-end/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -2,7 +2,7 @@ import FakeUsersRepository from "../repositories/fakes/FakeUsersRepository";
 import AuthenticateUserService from "./AuthenticateUserService";
 import CreateUserService from "./CreateUserService";
 import AppError from "@shared/errors/AppError";
-import FakeHashProvider  from "../providers/HashProvider/fakes/FakeHashProvider";
+import FakeHashProvider from "../providers/HashProvider/fakes/FakeHashProvider";
 
 describe('AuthenticateUser', () => {
 
@@ -32,6 +32,46 @@ describe('AuthenticateUser', () => {
 
         expect(response).toHaveProperty('token');
         expect(response.user).toBe(user);
+
+    });
+    it('should not be able to authenticate with non-existing email', async () => {
+        const fakeUsersRepository = new FakeUsersRepository();
+        const fakeHashProvider = new FakeHashProvider();
+
+        const authenticateUser = new AuthenticateUserService(
+            fakeUsersRepository,
+            fakeHashProvider
+        )
+
+        expect(authenticateUser.execute({
+            email: 'ops@example.com',
+            password: '123345',
+        })).rejects.toBeInstanceOf(AppError);
+
+    });
+    it('should not be able to authenticate with wrong password', async () => {
+        const fakeUsersRepository = new FakeUsersRepository();
+        const fakeHashProvider = new FakeHashProvider();
+
+        const createUser = new CreateUserService(
+            fakeUsersRepository,
+            fakeHashProvider
+        )
+        const authenticateUser = new AuthenticateUserService(
+            fakeUsersRepository,
+            fakeHashProvider
+        )
+
+        await createUser.execute({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: '123345',
+        });
+
+        expect(authenticateUser.execute({
+            email: 'johndoe@example.com',
+            password: 'asdsadadsa',
+        })).rejects.toBeInstanceOf(AppError);
 
     });
 });
