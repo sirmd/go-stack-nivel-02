8618d934783c953b0c7b18c27285679630a4581e
sirmd <mate.deitos@hotmail.com>
2020-07-22 17:59:15 -0300

    Nivel 04 -> NodeJs -> Salvando Tokens no Banco

--- back-end/src/shared/container/index.ts
+++ back-end/src/shared/container/index.ts
@@ -6,6 +6,7 @@ import AppointmentsRepository from "@modules/appointments/infra/typeorm/reposito
 import { IUsersRepository } from "@modules/users/repositories/IUsersRepository";
 import UsersRepository from "@modules/users/infra/typeorm/repositories/UsersRepository";
 import { IUserTokensRepository } from '@modules/users/repositories/IUserTokensRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 
 container.registerSingleton<IAppointmentsRepository>(
   'AppointmentsRepository',
@@ -16,7 +17,7 @@ container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository
 );
-// container.registerSingleton<IUserTokensRepository>(
-//   'UserTokensRepository',
-//   UserTokensRepository
-// );
+container.registerSingleton<IUserTokensRepository>(
+  'UserTokensRepository',
+  UserTokensRepository
+);
