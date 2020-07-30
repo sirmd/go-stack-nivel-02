4d363ed16be560d25ffdef3ca67e6c84dc4a307e
sirmd <mate.deitos@hotmail.com>
2020-07-30 20:33:53 -0300

    Nivel 04 -> NodeJS -> Enviando notificações ao MongoDB

--- back-end/src/modules/appointments/services/CreateAppointmentService.ts
+++ back-end/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,7 +1,8 @@
-import { startOfHour, isBefore, getHours } from 'date-fns';
+import { startOfHour, isBefore, getHours, format } from 'date-fns';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import AppError from '@shared/errors/AppError';
 import { inject, injectable } from 'tsyringe';
+import { INotificationsRepository } from '@modules/notifications/repositories/INotificationsRepository';
 import { IAppointmentsRepository } from '../repositories/IAppointmentsRepository';
 
 interface IRequestDTO {
@@ -15,6 +16,8 @@ class CreateAppointmentService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
+    @inject('NotificationsRepository')
+    private notificationsRepository: INotificationsRepository,
   ) {}
 
   public async execute({
@@ -48,6 +51,13 @@ class CreateAppointmentService {
       provider_id,
       user_id,
       date: appointmentDate,
+    });
+
+    const dateFormatted = format(appointmentDate, "dd/MM/yyyy 'às' HH:mm'h'");
+
+    await this.notificationsRepository.create({
+      recipient_id: provider_id,
+      content: `Novo agendamento para ${dateFormatted}`,
     });
 
     return appointment;
--- back-end/src/shared/container/index.ts
+++ back-end/src/shared/container/index.ts
@@ -7,6 +7,8 @@ import { IUsersRepository } from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import { IUserTokensRepository } from '@modules/users/repositories/IUserTokensRepository';
 import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
+import { INotificationsRepository } from '@modules/notifications/repositories/INotificationsRepository';
+import NotificationsRepository from '@modules/notifications/infra/typeorm/repositories/NotificationsRepository';
 
 container.registerSingleton<IAppointmentsRepository>(
   'AppointmentsRepository',
@@ -20,4 +22,8 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton<IUserTokensRepository>(
   'UserTokensRepository',
   UserTokensRepository,
+);
+container.registerSingleton<INotificationsRepository>(
+  'NotificationsRepository',
+  NotificationsRepository,
 );
