d39cd94fe1e5fa0f930de1c45e54f9be1f6dcef8
sirmd <mate.deitos@hotmail.com>
2020-08-04 23:09:27 -0300

    Nivel 04 -> NodeJS -> Enviando email com amazon ses

--- back-end/src/shared/providers/index.ts
+++ back-end/src/shared/providers/index.ts
@@ -1,8 +1,10 @@
 import { container } from 'tsyringe';
+import mailConfig from '@config/mail';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 import IMailProvider from './MailProvider/models/IMailProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
+import SesMailProvider from './MailProvider/implementations/SesMailProvider';
 import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
 import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
 
@@ -17,5 +19,7 @@ container.registerSingleton<IMailTemplateProvider>(
 
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  container.resolve(EtherealMailProvider),
+  mailConfig.driver === 'ethereal'
+    ? container.resolve(EtherealMailProvider)
+    : container.resolve(SesMailProvider),
 );
