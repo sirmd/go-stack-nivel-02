aae383e9241d810c7a99cb8a06f63766a59c5a9a
sirmd <mate.deitos@hotmail.com>
2020-08-05 22:08:20 -0300

    Nivel 04 -> NodeJS -> Enviando arquivos ao Amazon S3

--- back-end/src/modules/users/services/UpdateUserAvatarService.ts
+++ back-end/src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,11 +1,8 @@
-import path from 'path';
-import fs from 'fs';
 import User from '@modules/users/infra/typeorm/entities/User';
-import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
 import { inject, injectable } from 'tsyringe';
-import { IUsersRepository } from '../repositories/IUsersRepository';
 import IStorageProvider from '@shared/providers/StorageProvider/models/IStorageProvider';
+import { IUsersRepository } from '../repositories/IUsersRepository';
 
 interface RequestDTO {
   user_id: string;
@@ -14,18 +11,15 @@ interface RequestDTO {
 
 @injectable()
 class UpdateUserAvatarService {
-
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
 
     @inject('StorageProvider')
     private storageProvider: IStorageProvider,
-
-  ) { }
+  ) {}
 
   public async execute({ user_id, avatarFilename }: RequestDTO): Promise<User> {
-
     const user = await this.usersRepository.findById(user_id);
 
     if (!user) {
