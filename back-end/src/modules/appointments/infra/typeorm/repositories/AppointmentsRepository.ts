5e3ac11e792d0478e322a4c67cba3776c8c9c56c
sirmd <mate.deitos@hotmail.com>
2020-08-11 17:52:01 -0300

    Nivel 05 -> Ajustes no backend para fazer eager loading do usuário ao buscar os agendamenetos do dia

--- back-end/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ back-end/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -28,6 +28,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
             `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
+      relations: ['user'],
     });
 
     return appointments;
