d1ebe71d03810ea972f15d3f8612d792f1213884
sirmd <mate.deitos@hotmail.com>
2020-08-11 11:57:47 -0300

    Nivel 05 -> Ajustes no backend para utilizar query params nos métodos GET de Appointments

--- back-end/src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
+++ back-end/src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -4,7 +4,7 @@ import ListProviderDayAvailabilityService from '@modules/appointments/services/L
 
 export default class ProviderDayAvailabilityController {
   public async index(request: Request, response: Response): Promise<Response> {
-    const { year, month, day } = request.body;
+    const { year, month, day } = request.query;
     const { provider_id } = request.params;
 
     const listProviderDayAvailability = container.resolve(
@@ -13,9 +13,9 @@ export default class ProviderDayAvailabilityController {
 
     const appointments = await listProviderDayAvailability.execute({
       provider_id,
-      year,
-      month,
-      day,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year),
     });
 
     return response.json(appointments);
