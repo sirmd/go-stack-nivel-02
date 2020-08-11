adeea4dc8c00bdea57373d374880c4c34352f50f
sirmd <mate.deitos@hotmail.com>
2020-08-11 14:07:52 -0300

    Nivel 05 -> Ajustes no backend para verificar agendamentos no dia

--- back-end/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ back-end/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -5,7 +5,7 @@ import IFoundInDayFromProviderDTO from '../dtos/IFoundInDayFromProviderDTO';
 
 export interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
-  findByDate(date: Date): Promise<Appointment | undefined>;
+  findByDate(date: Date, provider_id: string): Promise<Appointment | undefined>;
   findAllInMonthFromProvider(
     data: IFoundInMonthFromProviderDTO,
   ): Promise<Appointment[]>;
